72c8722c8faa45c0ab0278bb54a1ebca34a33f17
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-01-06 19:58:26 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,14 +10,28 @@ const nanoid = customAlphabet('1234567890id', 4);
 
 export class App extends Component {
   state = {
-    contacts: [
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts')) || [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+    ];
+    this.setState({ contacts });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (
+      prevState.contacts.length !== 0 &&
+      prevState.contacts.length !== this.state.contacts.length
+    ) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addUser = data => {
     const newContact = {

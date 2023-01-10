6d8545d0a1fb423d0139d7c6b496bf5be26da235
Oleksandr Perlov <oleksandrperlov@gmail.com>
2023-01-10 14:51:24 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,20 +15,12 @@ export class App extends Component {
   };
 
   componentDidMount() {
-    const contacts = JSON.parse(localStorage.getItem('contacts')) || [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ];
+    const contacts = JSON.parse(localStorage.getItem('contacts')) || [];
     this.setState({ contacts });
   }
 
   componentDidUpdate(_, prevState) {
-    if (
-      prevState.contacts.length !== 0 &&
-      prevState.contacts.length !== this.state.contacts.length
-    ) {
+    if (prevState.contacts !== this.state.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
